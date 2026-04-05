72508477d47c88e53d839f6c1c92c54ea89b70f4
ashwinirg1319-bit <ashwinirg1319@gmail.com>
2026-04-05 21:26:14 +0530

    Updated run script file

--- scripts/run_test.bat
+++ scripts/run_test.bat
@@ -1,11 +1,11 @@
 @echo off
 set JMETER_PATH=C:\Users\ashwi\Downloads\apache-jmeter-5.6.3\apache-jmeter-5.6.3\bin
 
-REM Run test in non-GUI mode
-%JMETER_PATH%\jmeter -n -t ..\testplans\Certiplate_TestPlan.jmx -l ..\results\result.jtl
+REM Delete old report
+rmdir /s /q ..\results\report
 
-REM Generate HTML report from the .jtl file
-%JMETER_PATH%\jmeter -g ..\results\result.jtl -o ..\results\report
+REM Run test + generate report together
+%JMETER_PATH%\jmeter -n -t ..\testplans\Certiplate_Test_Plan.jmx -l ..\results\result.jtl -e -o ..\results\report
 
 echo HTML report generated at results\report
 pause
